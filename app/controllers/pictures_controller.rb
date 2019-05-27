f70afdef9dd352c244600b6a80354de5816353f9
Luca Carnessale <lcarness@chicagobooth.edu>
2019-05-27 16:38:41 +0000

    Test 1

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -18,4 +18,60 @@ class PicturesController < ApplicationController
 
     render("pic_templates/details.html.erb")
   end
+  
+  def byyyyeeeee
+    # Rails assigns:
+    #   params = {"id_to_delete"=>"628"}
+    
+    pic_id = params.fetch("id_to_delete")
+    
+    picture = Photo.where({ :id => pic_id }).at(0)
+    
+    picture.destroy
+    
+    redirect_to("/popular")
+  end
+  
+  def blank_form
+    @all_users = User.all.order({ :username => :asc })
+    
+    render("pic_templates/new_form.html.erb")
+  end
+  
+  def save_new_row
+    # Save the info...
+    
+    p = Photo.new
+    p.image = params.fetch("pic_image")
+    p.caption = params.fetch("pic_caption")
+    p.owner_id = params.fetch("poster_id")
+    
+    p.save
+    
+    redirect_to("/recent")
+  end
+  
+  def prefilled_form
+    pic_id = params.fetch("id_to_update")
+    
+    @photo = Photo.where({ :id => pic_id }).at(0)
+    
+    render("pic_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    
+    pic_id = params.fetch("id_to_update")
+    
+    p = Photo.where({ :id => pic_id }).at(0)
+    
+    p.image = params.fetch("pic_image")
+    p.caption = params.fetch("pic_caption")
+    
+    p.save
+    
+    redirect_to("/photos/" + pic_id)
+  end
+
+
 end
